ae7a3132d7818a54ef20c7a143525768b6b00286
R4y <152296800+d3vi0uss@users.noreply.github.com>
2026-02-19 16:28:19 -0600

    Add skin data to data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,6 @@
-
+const skins = [
+  { name: "AK-47 | Rusted Fury", rarity: "common", value: 10 },
+  { name: "M4A1-S | Night Ops", rarity: "rare", value: 50 },
+  { name: "AWP | Crimson Storm", rarity: "epic", value: 200 },
+  { name: "Karambit | Golden Blaze", rarity: "legendary", value: 1000 }
+];
